a78b0b3e6e45320c2db64b511d0ba5bc951648db
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2024-04-04 01:03:04 +0100

    successfully displayed the liquidity

--- index.js
+++ index.js
@@ -1,59 +1,158 @@
-const { Web3 } = require('web3');
+const { Web3 } = require("web3");
 
 async function listenForPairCreatedEvents() {
-    const NODE_URL = 'wss://ethereum-mainnet.core.chainstack.com/ws/673e14e04acb090649b2f13d15a74b34';
-    const web3 = new Web3(NODE_URL);
+  const NODE_URL =
+    "wss://ethereum-mainnet.core.chainstack.com/ws/673e14e04acb090649b2f13d15a74b34";
+  const web3 = new Web3(NODE_URL);
 
-    const UNISWAP_FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+  const UNISWAP_FACTORY_ADDRESS = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 
-    const factoryContract = new web3.eth.Contract([
+  const PairAbi = [
+    {
+      constant: true,
+      inputs: [],
+      name: "symbol",
+      outputs: [
         {
-            "anonymous": false,
-            "inputs": [
-                {
-                    "indexed": true,
-                    "internalType": "address",
-                    "name": "token0",
-                    "type": "address"
-                },
-                {
-                    "indexed": true,
-                    "internalType": "address",
-                    "name": "token1",
-                    "type": "address"
-                },
-                {
-                    "indexed": false,
-                    "internalType": "address",
-                    "name": "pair",
-                    "type": "address"
-                },
-                {
-                    "indexed": false,
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "name": "PairCreated",
-            "type": "event"
-        }
-    ], UNISWAP_FACTORY_ADDRESS);
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      payable: false,
+      stateMutability: "pure",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: "getReserves",
+      outputs: [
+        {
+          internalType: "uint112",
+          name: "reserve0",
+          type: "uint112",
+        },
+        {
+          internalType: "uint112",
+          name: "reserve1",
+          type: "uint112",
+        },
+        {
+          internalType: "uint32",
+          name: "blockTimestampLast",
+          type: "uint32",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "pure",
+        "type": "function"
+      },
+  ];
 
-    factoryContract.events.PairCreated({})
-        .on('data', event => {
-            // console.log("New Pair Detected");
-            console.log("event created", event)
-            // console.log("------------------------------------");
-            console.log("Transaction tx", event.transactionHash)
-         
-            console.log("------------------------------------");
-            console.log("New Pair Detected");
-            console.log("Token 0:", event.returnValues.token0);
-            console.log("Token 1:", event.returnValues.token1);
-            console.log("Pair Address:", event.returnValues.pair);
-            console.log("------------------------------------");
-        })      
+  const factoryContract = new web3.eth.Contract(
+    [
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: true,
+            internalType: "address",
+            name: "token0",
+            type: "address",
+          },
+          {
+            indexed: true,
+            internalType: "address",
+            name: "token1",
+            type: "address",
+          },
+          {
+            indexed: false,
+            internalType: "address",
+            name: "pair",
+            type: "address",
+          },
+          {
+            indexed: false,
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        name: "PairCreated",
+        type: "event",
+      },
+      {
+        constant: true,
+        inputs: [
+          { internalType: "address", name: "", type: "address" },
+          { internalType: "address", name: "", type: "address" },
+        ],
+        name: "getPair",
+        outputs: [{ internalType: "address", name: "", type: "address" }],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    UNISWAP_FACTORY_ADDRESS
+  );
+
+  factoryContract.events.PairCreated({}).on("data", async (event) => {
+    console.log("New Pair Detected");
+    console.log("------------------------------------");
+    console.log("New Pair Detected");
+    console.log("Token 0:", event.returnValues.token0);
+    console.log("Token 1:", event.returnValues.token1);
+    console.log("Pair Address:", event.returnValues.pair);
+
+    const Token0 = event.returnValues.token0;
+    const Token1 = event.returnValues.token1;
+
+    factoryContract.methods
+      .getPair(Token0, Token1)
+      .call()
+      .then((pair) => {
+        console.log("Pair Address from getPair:", pair);
+
+        if (pair) {
+          var pairContract = new web3.eth.Contract(PairAbi, pair);
+
+          pairContract.methods
+            .getReserves()
+            .call()
+            .then((reserves) => {
+              console.log("Pair Reserves: ", reserves);
+
+              pairContract.methods.name().call().then((name) => {
+                console.log("Pair Name: ", name);
+              })
+              .catch((error) => {
+                console.error("Error fetching name:", error);
+              });
+            })
+            .catch((error) => {
+              console.error("Error fetching reserves:", error);
+            });
+        }
+      });
+  });
 }
 
 listenForPairCreatedEvents();
